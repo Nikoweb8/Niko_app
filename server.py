9c2a94bf855b1e1540b98d70c35798fe3e7d6b80
Nikoweb8 <nikoweb8@proton.me>
2023-12-30 20:37:23 +0100

    Level Up🟡

--- server.py
+++ server.py
@@ -28,3 +28,27 @@ def my_photo():
 def my_Results():
     return render_template('Results.html')
 
+
+@app.route('/2019.html')
+def my_2019():
+    return render_template('2019.html')
+
+@app.route('/2020.html')
+def my_2020():
+    return render_template('2020.html')
+
+@app.route('/2021.html')
+def my_2021():
+    return render_template('2021.html')
+
+@app.route('/2022.html')
+def my_2022():
+    return render_template('2022.html')
+
+@app.route('/2023.html')
+def my_2023():
+    return render_template('2023.html')
+
+@app.route('/NewsPaper.html')
+def my_NewsPaper():
+    return render_template('NewsPaper.html')
